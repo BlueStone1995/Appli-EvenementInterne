3accbb4dd2b958aee0167dc68349385d9030eef4
jorzagau <47818064+jorzagau@users.noreply.github.com>
2019-03-17 18:01:55 +0100

    Update EpargneListener.java

--- src/appli/banqueJamasse/evenement/EpargneListener.java
+++ src/appli/banqueJamasse/evenement/EpargneListener.java
@@ -24,13 +24,22 @@ public class EpargneListener implements PropertyChangeListener {
         boolean res = false;
         if (evt.getPropertyName().equals("epargne")) {
             CompteEpargne c = (CompteEpargne) evt.getSource();
-            Integer oldValue = (Integer) evt.getOldValue();
-            Integer newValue = (Integer) evt.getNewValue();
-            // TODO : Implémenter condition métier
-            // res = (oldValue > s && (newValue <= s));
+            Float oldValue = (Float) evt.getOldValue();
+            Float newValue = (Float) evt.getNewValue();
+            Float s = cc.getSeuilMax();
+            res = (oldValue<=s && (newValue>s));
         }
+        else 
+            if (evt.getPropertyName().equals("seuilMax")) {
+                CompteCourant cc = (CompteCourant) evt.getSource();
+                Float oldValue=(Float) evt.getOldValue();
+                Float newValue=(Float) evt.getNewValue();
+                Float solde = cc.getSolde();
+                res=((solde<oldValue) && (solde>=newValue)); 
+        }    
         return res;
     }
+
 
     private void trigger_evt(PropertyChangeEvent evt) {
 
@@ -40,5 +49,26 @@ public class EpargneListener implements PropertyChangeListener {
         System.out.println("property " + evt.getPropertyName() + " old " + evt.getOldValue() + " new " + evt.getNewValue());
 
         // TODO : Implementer logique métier
+        if (evt.getPropertyName().equals("solde")) System.out.println("Augmentation  de solde");
+        else System.out.println("Baisse de seuil maximum");
+        DateFormat fd = new SimpleDateFormat("dd/MM/yyyy");
+        String d = fd.format(Calendar.getInstance().getTime());
+        
+        CompteCourant cc = ((CompteCourant)evt.getSource());
+        CompteEpargne ec = cc.getCompteForcement();
+        
+        Float montant = cc.getSolde()-cc.getSeuilMax();
+        
+        TypeOperation t = TypeOperation.VIREMENT;
+        
+        Operation op1 = new Operation(context.getMaxIdOperation()+1, cc, d, -montant,"Débit compte courant :"+c, t);
+        context.addOperationBancaire(op1);
+        cc.debiter(montant);
+        
+        Operation op2 = new Operation(context.getMaxIdOperation()+1, ec, d, montant, "Crédit compte épargne : "+c, t);
+        context.addOperationBancaire(op2);
+        ec.crediter(montant);
+       
+        
     }
 }
